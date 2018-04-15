cc8b7bea63037a8028bc61aaf56bb3b9eb8bef33
AlexanderAnishchik <sanyaaxel94@gmail.com>
2018-04-15 22:02:30 +0300

    init

---
+++ signalr/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace signalr
+{
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+    }
+    public class ChatHub : Hub
+    {
+        public Task Send(Message data)
+        {
+            return Clients.All.SendAsync("SendMessage", data.User, data.MessageText, UserHandler.ConnectedIds.Count);
+        }
+        public override Task OnConnectedAsync()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception ex)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(ex);
+        }
+    }
+}
